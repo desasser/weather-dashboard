54678200a6fd477cee2cda719560447ff3b3950d
Daniel Sasser <desasser79@gmail.com>
2020-12-18 16:19:37 -0800

    built skeleton to begin psuedocode

---
+++ script.js
@@ -0,0 +1,27 @@
+//TODO: Provide form for users to input data
+    //TODO: Input for city
+
+//TODO: Fetch current and future conditions for city
+    //TODO: BONUS: Button to toggle between metric and imperial
+//TODO: Current city results should present
+    //TODO: City Name
+    //TODO: Date
+    //TODO: Icon for weather conditions (font-awesome or weatherapi?)
+    //TODO: Temp
+    //TODO: Humidity
+    //TODO: Wind speed
+    //TODO: UV Index
+        //TODO: Coloration for if conditions are favorable (green), moderate (yellow), or severe (red)
+    //TODO: BONUS: Air Pollution with color scale
+
+//TODO: Future weather results should present
+    //TODO: Five day forecast with
+        //TODO: Date
+        //TODO: Icon for weather (FA/weather api)
+        //TODO: Temp
+        //TODO: Humidity
+
+//TODO: Add city to search history and store for later searching
+//TODO: Add click functionality to search history to view conditions again
+//TODO: When opening site, should display last searched city forecast
+
