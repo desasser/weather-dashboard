621766ea41fa3c7f0006599617f15c97e8cb0251
Daniel Sasser <desasser79@gmail.com>
2020-12-20 19:46:05 -0800

    functionality for output improved

--- script.js
+++ script.js
@@ -1,126 +1,134 @@
-//TODO: Provide form for users to input data
-    //TODO: Input for city inside of form
-    //TODO: Search button functionality as well with magnifying glass image
-
 //TODO: Fetch current and future conditions for city
-    //TODO: BONUS: Button to toggle between metric and imperial
-//TODO: Current city results should present
-
-        //TODO: Coloration for if conditions are favorable (green), moderate (yellow), or severe (red)
-    //TODO: BONUS: Air Pollution with color scale
 
 //TODO: Future weather results should present
-    //TODO: Five day forecast with
-        //TODO: Date
-        //TODO: Icon for weather (FA/weather api)
-        //TODO: Temp
-        //TODO: Humidity
+//TODO: Five day forecast with
+//TODO: Date
+//TODO: Icon for weather (FA/weather api)
+//TODO: Temp
+//TODO: Humidity
 
-//TODO: Add city to search history and store for later searching
-    //TODO: BONUS: Toggle functionality to hide and expand search history
+//declare variables for button click/submit event
+var thisSearch = '';
+//TODO: Bring this back
+// var searchHistArr = JSON.parse(localStorage.getItem("history")) || [];
+var liEl = $("<li>");
 
-    //declare variables for button click/submit event
-    var thisSearch = '';
-    //TODO: Bring thas back
-    // var searchHistArr = JSON.parse(localStorage.getItem("history")) || [];
+var currentTime = moment().format('HH');
+var now = moment().format('dddd, MM/DD/YY');
+
+
+//TODO: Bonus Stuff
+//How do I get this to work on both the button and the input at the same time?
+//Option 1 - make guts a function and call the function with two .on commands
+//Option 2 - direct everything within the form to the same thing if XX happens
+//Option 3 - Write the same code twice with two trigger commands
+//If I use a form, the button ends up on the second line
+//$("form").on("submit", function(event)
+//How to set dark/light mode?
+//I'd like to have the card background dark with white text at night
+//and light with dark text during the day (based on if statement)
+//Button to toggle between metric and imperial
+//Toggle functionality to hide and expand search history
+//Air Pollution with color scale
+//Add a doppler from the area you are searching
+$("#button-search").on("click", function (event) {
+    //prevent refresh on submit
+    event.preventDefault();
+
+    //if input is blank, return without doing anything
+    if ($("#input-flavor").val() === '') {
+        return;
+    }
+
+    //save input text and convert to first letter capitalized
+    thisSearch = $("#input-flavor").val();
+    thisSearch = thisSearch.toLowerCase();
+
+    //snippet found from stack exchange
+    thisSearch = thisSearch.charAt(0).toUpperCase() + thisSearch.slice(1);
+
+
+    //append to page in .list-group
     var liEl = $("<li>");
+    $(liEl).text(thisSearch)
+    $(liEl).addClass("list-group-item")
+    $(".list-group").append(liEl);
 
-    var now = moment().format('dddd, MMMM Do YYYY');
+    //TODO: Bring thas back
+    //push latest search into array
+    // searchHistArr.push(thisSearch);
+    // console.log(searchHistArr);
+    //TODO: Bring thas back
+    // localStorage.setItem("history", JSON.stringify(searchHistArr));
 
-    //TODO: How do I get this to work on both the button and the input at the same time?
-        //Option 1 - make guts a function and call the function with two .on commands
-        //Option 2 - direct everything within the form to the same thing if XX happens
-        //Option 3 - Write the same code twice with two trigger commands
-    //TODO: If I use a form, the button ends up on the second line
-        //$("form").on("submit", function(event)
+    //clear input field
+    $("#input-flavor").val('')
 
-    $("#button-search").on("click", function(event) {
-        //prevent refresh on submit
-        event.preventDefault();
+    //call weather API to pull up weather info from openweather API
+    //TODO: Can add back in alerts if needed
+    var queryURLCityCall = `https://api.openweathermap.org/data/2.5/weather?q=${thisSearch}&appid=677d25f5725630dee0d3fb96edd1516f`
+    // console.log(queryURLCityCall);
 
-        //if input is blank, return without doing anything
-        if ($("#input-flavor").val() === '') {
-            return;
-        }
-        
-        //save input text
-        thisSearch = $("#input-flavor").val();
-        
+    //ajax call utilizing current weather API to extract lat/long data
+    //save lat/long into var coords
+    //feed coords into One Call to get current/future forecast
+    $.ajax({
+        url: queryURLCityCall,
+        method: "GET"
+    }).then(function (response) {
+        // console.log(response);
 
-        //append to page in .list-group
-        var liEl = $("<li>");
-        $(liEl).text(thisSearch)
-        $(liEl).addClass("list-group-item")
-        $(".list-group").append(liEl);
-        
-        //TODO: Bring thas back
-        //push latest search into array
-        // searchHistArr.push(thisSearch);
-        // console.log(searchHistArr);
-        //TODO: Bring thas back
-        // localStorage.setItem("history", JSON.stringify(searchHistArr));
-        
-        //clear input field
-        $("#input-flavor").val('')
+        var lat = response.coord.lat;
+        var long = response.coord.lon;
+        // console.log(lat,long);
 
-        //call weather API to pull up weather info from openweather API
-        //TODO: Can add back in alerts if needed
-        var queryURLCityCall = `https://api.openweathermap.org/data/2.5/weather?q=${thisSearch}&appid=677d25f5725630dee0d3fb96edd1516f`
-        // console.log(queryURLCityCall);
+        var queryURLOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=miutely,hourly,alerts&units=imperial&appid=677d25f5725630dee0d3fb96edd1516f`;
+        // console.log(queryURLOneCall);
 
-        //ajax call utilizing current weather API to extract lat/long data
-        //save lat/long into var coords
-        //feed coords into One Call to get current/future forecast
+        //ajax call taking coords from previous call and feeding them into the onecall functionality of the weather API to extract all necessary data
         $.ajax({
-            url: queryURLCityCall,
+            url: queryURLOneCall,
             method: "GET"
-        }).then(function(response) {
-            // console.log(response);
+        }).then(function (responseToo) {
+            //declare variables for 
+            var icon = responseToo.current.weather[0].icon;
+            var iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+            var temp = responseToo.current.temp;
+            var humidity = responseToo.current.humidity;
+            var windSpd = responseToo.current.wind_speed;
+            var windDir = responseToo.current.wind_deg;
+            var uvInd = responseToo.current.uvi;
+            var iconImage = $("<img>");
+            iconImage.attr('src', iconURL)
 
-            var lat = response.coord.lat;
-            var long = response.coord.lon;
-            // console.log(lat,long);
+            $("#city-name").text(thisSearch + ' -- ' + now)
+            $("#city-name").append(iconImage);
+            $("#p-temp").text(`Temperature: ${temp} \u00b0F`);
+            $("#p-humid").text(`Humidity: ${humidity}% humidity`);
+            $("#p-wind-speed").text(`Wind Speed: ${windSpd} mph`);
+            //TODO: convert wind direction into North, South, East, West
 
-            var queryURLOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=miutely,hourly,alerts&appid=677d25f5725630dee0d3fb96edd1516f`;
-            // console.log(queryURLOneCall);
-
-            //ajax call taking coords from previous call and feeding them into the onecall functionality of the weather API to extract all necessary data
-            $.ajax({
-                url: queryURLOneCall,
-                method: "GET"
-            }).then(function(responseToo){
-                //declare variables for 
-                var icon = responseToo.current.weather[0].icon;
-                var iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
-                $("#other-space").append(iconURL);
-                var temp = responseToo.current.temp;
-                var humidity = responseToo.current.humidity;
-                var windSpd = responseToo.current.wind_speed;
-                var windDir = responseToo.current.wind_deg;
-                var uvInd = responseToo.current.uvi;
-
-                $("#p-temp").text(temp);
-                $("#p-humid").text(humidity);
-                $("#p-wind-speed").text(windSpd);
-                $("#uv-score").text(windSpd);
-
-                console.log(responseToo);
-                console.log(thisSearch);
-                console.log(now);
-                console.log(icon);
-                console.log('temp', temp);
-                console.log('humidity', humidity);
-                console.log('wind speed', windSpd);
-                //TODO: convert wind direction into North, South, East, West
-                console.log('wind direction', windDir);
-                console.log('uv index', uvInd);
-                                
-            })
-
+            //Set up style for UV index and change the background color of the container based on value
+            //TODO: Don't want it to be a button
+            var uvIndSpan = $("<span>");
+            uvIndSpan.text(uvInd);
+            uvIndSpan.addClass('uv-style');
+            $("#uv-score").text("UV Index: ")
+            if (uvInd > 6) {
+                uvIndSpan.css("background-color", "FF4848")
+            } else if (3 < uvInd >= 6) {
+                uvIndSpan.css("background-color", "#FFFF66")
+            } else {
+                uvIndSpan.css("background-color", "#ADFF2F")
+            }
+            $("#uv-score").append(uvIndSpan);
         })
 
     })
 
+})
+
+    //When opening site, should display last searched city forecast
     //TODO: Clear results button
     // remove the clear button when the button is clicked again
     // $(".clear-button-wrapper").empty();
@@ -140,7 +148,6 @@
         // }
 
 //TODO: Add click functionality to search history to view conditions again
-//TODO: When opening site, should display last searched city forecast
 
-//TODO: BONUS BONUS: Add a dopler from the area you are searching
+
 
